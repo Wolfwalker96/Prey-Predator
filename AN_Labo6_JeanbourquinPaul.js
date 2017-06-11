b0f75b7b01e512313f4b1ea4daafceb4345f4975
paul.jeanbour <pa.jeanbourquin@gmail.com>
2017-06-12 00:50:57 +0200

    Comment + README + Report

--- AN_Labo6_JeanbourquinPaul.js
+++ AN_Labo6_JeanbourquinPaul.js
@@ -3,12 +3,30 @@
  *   	Paul Jeanbourquin, INF2dlm-a.
  *    15.05.2017
  */
- 
+
+ /*
+  *   simultate(prey_born, prey_mortality, predator_born, predator_mortality, init_pred, init_prey, h, n)
+  *     prey_born :         natality coeficient of preys.
+  *     prey_mortality:     mortality coeficient of preys.
+  *     predator_born :     natality coeficient of predators.
+  *     predator_mortality: mortality coeficient of predators.
+  *     init_pred :         initial number of predators.
+  *     init_prey :         initial number of prey.s
+  *     h :                 time difference.
+  *     n :                 iteration number.
+  */
+
 function simulate(prey_born, prey_mortality, predator_born, predator_mortality, init_pred, init_prey, h, n){
 
 	// Alogirthm.
 
 	// Init SDDs and variables.
+
+  /*
+   *  As we can't use float number as array indexes, I decide to use three array.
+   *  Datas which are connected are to the same index.
+   *    Example : prey[j] is prey(time[j]) (same for predator).
+   */
 
 	let prey = [];
 	let predator = [];
@@ -23,16 +41,21 @@ function simulate(prey_born, prey_mortality, predator_born, predator_mortality, 
 	// t>0.
 
 	for(let i=1;i<n;i++){
+    // Prey(t=i*h).
 		nprey = prey[i-1] + h * ((prey_born*prey[i-1]) - (prey_mortality*prey[i-1]*predator[i-1]));
 		prey.push(nprey);
+
+    // Predator(t=i*h).
 		npredator = predator[i-1] + h * ( (predator_born*predator[i-1]*prey[i-1]) - (predator_mortality*predator[i-1]));
 		predator.push(npredator);
+
+    // Time(t=i*h).
 		time.push(i*h);
 	}
 
-	// Return.
+	// Return section.
 
-	let ret = [];
+	let ret = {};
 	ret['prey'] = prey;
 	ret['predator'] = predator;
 	ret['time'] = time;
--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# Prey-Predator
+Numerical Algorithm - 6nd Laboratory Prey-Predator
+==================================================
+
+* Author : Paul Jeanbourquin, INF2dlm-a
+* School : HE-Arc - Ingénierie (Neuchâtel, Switzerland)
+* Date : 12th June, 2017
+* [Online README](https://wolfwalker96.github.io/Prey-Predator/)
+* [Online version](https://wolfwalker96.github.io/Prey-Predator/AN_Labo6_JeanbourquinPaul.html)
+
+Objectives
+----------
+
+The objectives of this project is the implementation of the Prey-Predator
+mathematical model in a programs written in JavaScript.
+
+Method
+------
+
+### Differential equation solvation
+
+I use the Euler method for estimate the values of the differential equation from
+the models. For store the data, I decide to use three table linked together by
+the index of their values.
+
+User manual
+-------------
+
+### Simulation configuration
+
+The user can configure the simulation by using the input in the middle bloc.
+
+Files
+-----
+
+* **AN_Labo6_JeanbourquinPaul.html** Program view
+* **AN_Labo6_JeanbourquinPaul.js**  Script
+* **AN_Labo6_JeanbourquinPaul.pdf** Report of the laboratory
+* **js/** Some useful libraries
+
+Conclusion
+----------
+
+The simulation is rightly implemented. It gives the hoped values
+with a little inaccuracy due to the Euler method.
+
+To improve this program, we can use an more accurate method (as Runge for example).
